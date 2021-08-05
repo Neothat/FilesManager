ea76b045a37a69707d677c0558b36843c62c96e8
denis.volkov <denis.volkov@accenture.com>
2021-08-05 12:34:31 +0300

    The table is fully configured

--- src/main/java/ru/geekbrains/Controller.java
+++ src/main/java/ru/geekbrains/Controller.java
@@ -1,20 +1,20 @@
 package ru.geekbrains;
 
 import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
@@ -31,14 +31,42 @@ public class Controller implements Initializable {
 
         TableColumn<FileInfo, String> fileNameColumn = new TableColumn<>("Имя");
         fileNameColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFileName()));
-        fileNameColumn  .setPrefWidth(240);
+        fileNameColumn.setPrefWidth(240);
 
-        filesTable.getColumns().addAll(fileTypeColumn, fileNameColumn);
+        TableColumn<FileInfo, Long> fileSizeColumn = new TableColumn<>("Размер");
+        fileSizeColumn.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue().getSize()));
+        fileSizeColumn.setCellFactory(column -> {
+            return new TableCell<FileInfo, Long>(){
+                @Override
+                protected void updateItem(Long item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || empty){
+                        setText(null);
+                        setStyle("");
+                    } else {
+                        String text = String.format("%,d bytes", item);
+                        if (item == -1L){
+                            text = "[DIR]";
+                        }
+                        setText(text);
+                    }
+                }
+            };
+        });
+        fileNameColumn.setPrefWidth(120);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        TableColumn<FileInfo, String> fileDateColumn = new TableColumn<>("Дата изменения");
+        fileDateColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getLastModified().format(dtf)));
+        fileDateColumn.setPrefWidth(120);
+
+        filesTable.getColumns().addAll(fileTypeColumn, fileNameColumn, fileSizeColumn, fileDateColumn);
+        filesTable.getSortOrder().add(fileTypeColumn);
 
         updateList(Paths.get("C:/Users/denis.volkov/FilesManager"));
     }
 
-    public void updateList(Path path){
+    public void updateList(Path path) {
         try {
             filesTable.getItems().clear();
             filesTable.getItems().addAll(Files.list(path).map(FileInfo::new).collect(Collectors.toList()));
