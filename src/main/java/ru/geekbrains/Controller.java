0b8048b3b92fa012a70c16f2fd8ab024b727c0e7
denis.volkov <denis.volkov@accenture.com>
2021-08-05 11:16:28 +0300

    Action on exit buttons and binding a table to a controller

--- src/main/java/ru/geekbrains/Controller.java
+++ src/main/java/ru/geekbrains/Controller.java
@@ -1,4 +1,25 @@
 package ru.geekbrains;
 
-public class Controller {
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    public TableView filesTable;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void btnExitAction(ActionEvent actionEvent) {
+        Platform.exit();
+    }
 }
